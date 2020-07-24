256adcf3b20a615aab077bc4e214963f23cd6230
noorodin <n.kiani@outlook.com>
2020-07-24 19:11:13 +0430

    Handle not found users in UserInfo component.

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -3,14 +3,21 @@ import './UserInfo.css';
 
 function UserInfo({ user }) {
   return (
-    <article className="user">
-      <img className="avatar" src={user.avatar} alt={user.first_name}></img>
-      <div className="user-name">
-        <span className="first-name">{user.first_name}</span>
-        <span>{user.last_name}</span>
-      </div>
-      <span>{user.email}</span>
-    </article>
+    <>
+      {
+        user ?
+          <article className="user">
+            < img className="avatar" src={user.avatar} alt={user.first_name} ></img >
+            <div className="user-name">
+              <span className="first-name">{user.first_name}</span>
+              <span>{user.last_name}</span>
+            </div>
+            <span>{user.email}</span>
+          </article >
+          :
+          <h1>USER NOT FOUND!</h1>
+      }
+    </>
   );
 }
 
