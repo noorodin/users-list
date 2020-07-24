0321040819a9a327053eb9271309f41b0a49f64f
noorodin <n.kiani@outlook.com>
2020-07-24 12:45:44 +0430

    Router was added to project. Users component was created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Users from './components/Users/Users';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <div className="home-page">
+        <Switch>
+          <Route path="/">
+            <Users />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
