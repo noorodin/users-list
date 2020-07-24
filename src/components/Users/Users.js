59bf1b26e0d9f5703e68fe986b0e52bb6030318c
noorodin <n.kiani@outlook.com>
2020-07-24 13:17:13 +0430

    Users component fetchs data correctly.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 function Users() {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    let apiParameters = {
+      "page": 2,
+      "per_page": 3,
+    };
+    getUsersList(apiParameters);
+  }, []);
+
+  const getUsersList = ({ page, per_page }) => {
+    setLoading({ loading: true });
+
+    fetch(`https://reqres.in/api/users?page=${page}&per_page=${per_page}`)
+      .then(res => res.json())
+      .then(
+        (res) => {
+          console.log("response: ", res);
+          setLoading(false);
+        },
+        (err) => {
+          console.log("error: ", err);
+          setLoading(false);
+        }
+      )
+  }
+
   return (
     <div>
-      Users
+      {
+        loading ?
+          <div className="loading">Loading ...</div>
+          :
+          <div>Users</div>
+      }
     </div>
   );
 }
