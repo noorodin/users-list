aca7aa36991c4bd748a44261ffd416b08b377021
noorodin <n.kiani@outlook.com>
2020-07-24 19:17:15 +0430

    Updates

---
+++ precache-manifest.e1c54fbc0d8494be2800d7fb15790117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff170e85bc94ece6dd1b01621554a9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5805617cff235dcd5268",
+    "url": "/static/css/main.d4362941.chunk.css"
+  },
+  {
+    "revision": "5d6ee805253d57778765",
+    "url": "/static/js/2.df4bcac2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.df4bcac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5805617cff235dcd5268",
+    "url": "/static/js/main.8079c515.chunk.js"
+  },
+  {
+    "revision": "ce02b4d83709bae1d85d",
+    "url": "/static/js/runtime-main.5566532b.js"
+  }
+]);
